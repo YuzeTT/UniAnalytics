75e898f7ae0048fc2e1a987a26c8fb943f2b9fa5
YuzeTT <ziv1024@163.com>
2022-08-26 18:46:13 +0800

    add workflows

--- build.bat
+++ build.bat
@@ -1,9 +1,14 @@
-SET CGO_ENABLED=0
-SET GOOS=darwin
+SET CGO_ENABLED=1
+SET GOOS=windows
 SET GOARCH=amd64
 go build -o UniAnalytics_windows_amd64.exe main.go
 
-SET CGO_ENABLED=0
+SET CGO_ENABLED=1
+SET GOOS=darwin
+SET GOARCH=amd64
+go build -o UniAnalytics_macos_amd64 main.go
+
+SET CGO_ENABLED=1
 SET GOOS=linux
 SET GOARCH=amd64
 go build -o UniAnalytics_linux_amd64 main.go
